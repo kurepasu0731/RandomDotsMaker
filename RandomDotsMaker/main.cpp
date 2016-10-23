e130059fc90e91f2f34206871c1a99ce3c73888d
kurepasu0731 <kurepasu0731@gmail.com>
2016-10-23 23:06:52 +0900

    ブラーもどき

--- RandomDotsMaker/main.cpp
+++ RandomDotsMaker/main.cpp
@@ -9,11 +9,14 @@
 #include<iostream> //入出力ライブラリ
 #include<fstream> //iostreamのファイル入出力をサポート
 
-#define DOTNUM 150
+#define DOTNUM 80
 #define DOTDIST 30
-#define DOTSIZE 30 //直径
+#define DOTSIZE 10 //直径
 #define PROJECTOR_WIDTH 1280
 #define PROJECTOR_HEIGHT 800
+
+#define USEPINHOLE 1 //ピンホールするかどうか
+#define PINHOLERAD 200 //ピンホールの直径の最大
 
 #define OFFSET_X 10
 #define OFFSET_Y 10
@@ -29,7 +32,10 @@ void saveDots(std::string filename);
 int main(int argc, char** argv)
 {
 	//黒色画像生成
-	cv::Mat dstImg(cv::Size(PROJECTOR_WIDTH, PROJECTOR_HEIGHT), CV_8UC1, cv::Scalar(0, 0, 0));
+	cv::Mat dstImg(cv::Size(PROJECTOR_WIDTH, PROJECTOR_HEIGHT), CV_8UC4, cv::Scalar(0, 0, 0, 255));
+
+	//Pinholeの中心
+	cv::Point center(PROJECTOR_WIDTH / 2, PROJECTOR_HEIGHT / 2);
 
 	//生成範囲
 	int minX = DOTSIZE/2 + OFFSET_X;
@@ -44,6 +50,15 @@ int main(int argc, char** argv)
 		cv::Point p(x, y);
 
 		bool valid = true;
+
+		if(USEPINHOLE)
+		{
+			if(getDistance(center, p) < (PINHOLERAD + DOTSIZE/2))
+			{
+				continue;
+			}
+		}
+
 		for(int i = 0; i < dots.size(); i++)
 		{
 			if(getDistance(p, dots[i]) < DOTDIST + DOTSIZE)
@@ -57,11 +72,31 @@ int main(int argc, char** argv)
 		else dots.push_back(p);
 	}
 
-	//描画
+	if(USEPINHOLE)
+	{
+		//アルファにブラーもどきをかける
+		for(int y = 0 ; y < PROJECTOR_HEIGHT; y++)
+		{
+			for(int x = 0; x < PROJECTOR_WIDTH; x++)
+			{
+				cv::Point p(x, y);
+				double d = getDistance(center, p);
+				if(d < PINHOLERAD)
+				{
+					dstImg.data[y * dstImg.step + x * dstImg.elemSize() + 3] = 255 * d / PINHOLERAD;
+				}
+			}
+		}
+	}
+
+	//ドットの描画
 	for(int i = 0; i < dots.size(); i++)
 	{
-		cv::circle(dstImg, dots[i], DOTSIZE/2, cv::Scalar(255, 255, 255), -1, CV_AA);
+		cv::circle(dstImg, dots[i], DOTSIZE/2, cv::Scalar(255, 255, 255, 255), -1, CV_AA);
 	}
+
+	//Debug
+	//cv::circle(dstImg, center, PINHOLERAD , cv::Scalar(0, 0, 255, 255), 1);
 
 	//pngで保存
 	cv::imwrite("dots.png", dstImg);
